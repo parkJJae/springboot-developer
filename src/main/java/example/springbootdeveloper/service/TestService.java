e8d66c33e108ad83fc67814580df4ccc0aa5f852
parkJJae <wgpark0830@naver.com>
2025-05-18 23:22:54 +0900

    Chapter2

---
+++ src/main/java/example/springbootdeveloper/service/TestService.java
@@ -0,0 +1,19 @@
+package example.springbootdeveloper.service;
+
+import example.springbootdeveloper.entity.Member;
+import example.springbootdeveloper.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TestService {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    public List<Member> getAllMembers() {
+        return memberRepository.findAll();
+    }
+}
