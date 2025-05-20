cf499b99438bbbfb52c12934e5577096f44b748a
parkJJae <wgpark0830@naver.com>
2025-05-20 16:56:09 +0900

    HTML 뷰까지 완성 추후 삭제 , 수정기능 추가

---
+++ src/main/java/example/springbootdeveloper/controller/BlogViewController.java
@@ -0,0 +1,39 @@
+package example.springbootdeveloper.controller;
+
+import example.springbootdeveloper.domain.Article;
+import example.springbootdeveloper.dto.ArticleListViewResponse;
+import example.springbootdeveloper.dto.ArticleViewResponse;
+import example.springbootdeveloper.service.BlogService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Controller
+public class BlogViewController {
+    private final BlogService blogService;
+
+    @GetMapping("/articles")
+    public String getArticles(Model model) {
+        List<ArticleListViewResponse> articles = blogService.findAll().stream()
+                .map(ArticleListViewResponse::new)
+                .toList();
+        model.addAttribute("articles", articles);
+
+        return "articleList";
+    }
+
+    @GetMapping("articles/{id}")
+    public String getArticle(@PathVariable Long id, Model model) {
+        Article article = blogService.findById(id);
+        model.addAttribute("article", new ArticleViewResponse(article));
+
+        return "article";
+    }
+    
+}
---
+++ src/main/java/example/springbootdeveloper/dto/ArticleListViewResponse.java
@@ -0,0 +1,18 @@
+package example.springbootdeveloper.dto;
+
+import example.springbootdeveloper.domain.Article;
+import lombok.Getter;
+
+@Getter
+public class ArticleListViewResponse {
+
+    private final Long id;
+    private final String title;
+    private final String content;
+
+    public ArticleListViewResponse(Article article) {
+        this.id = article.getId();
+        this.title = article.getTitle();
+        this.content = article.getContent();
+    }
+}
