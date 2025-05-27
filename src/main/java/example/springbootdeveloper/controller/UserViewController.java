6cdd82535fa0457ad4d9510ebb1fe00f5fb31a33
parkJJae <wgpark0830@naver.com>
2025-05-27 16:44:51 +0900

    로그인/로그아웃 기능 구현

--- src/main/java/example/springbootdeveloper/controller/UserViewController.java
+++ src/main/java/example/springbootdeveloper/controller/UserViewController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserViewController {
-//    @GetMapping("/login")
-//    public String login() {
-//        return "login";
-//    }
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
 
     @GetMapping("/signup")
     public String signup() {
